bd86f2844a84c4373cdf8eb3bffd61830a79a2c1
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-03-20 13:57:23 +0300

    task SortedByAbsoluteValueIntegerSet part 1

--- src/main/java/com/efimchick/ifmo/collections/SortedByAbsoluteValueIntegerSet.java
+++ src/main/java/com/efimchick/ifmo/collections/SortedByAbsoluteValueIntegerSet.java
@@ -1,31 +1,7 @@
 package com.efimchick.ifmo.collections;
 
-import java.util.*;
+import java.util.HashSet;
 
-class SortedByAbsoluteValueIntegerSet extends TreeSet<Integer> {
+class SortedByAbsoluteValueIntegerSet extends HashSet<Integer> {
 
-    private Set<Integer> set;
-
-    SortedByAbsoluteValueIntegerSet() {
-        set = new TreeSet<>();
-    }
-    @Override
-    public Comparator<? super Integer> comparator() {
-        return null;
-    }
-
-    @Override
-    public TreeSet<Integer> subSet(Integer fromElement, Integer toElement) {
-        return null;
-    }
-
-    @Override
-    public TreeSet<Integer> headSet(Integer toElement) {
-        return null;
-    }
-
-    @Override
-    public TreeSet<Integer> tailSet(Integer fromElement) {
-        return null;
-    }
 }
