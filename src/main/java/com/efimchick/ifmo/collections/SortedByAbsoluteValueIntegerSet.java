ab05a6cd12c27f72cba40340a4680227093239c9
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-03-25 09:47:46 +0300

    SortedByAbsoluteValueIntegerSet task

--- src/main/java/com/efimchick/ifmo/collections/SortedByAbsoluteValueIntegerSet.java
+++ src/main/java/com/efimchick/ifmo/collections/SortedByAbsoluteValueIntegerSet.java
@@ -1,9 +1,61 @@
 package com.efimchick.ifmo.collections;
 
-import java.util.HashSet;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-class SortedByAbsoluteValueIntegerSet extends HashSet<Integer> {
+class SortedByAbsoluteValueIntegerSet extends LinkedHashSet<Integer>  {
 
     private static final long serialVersionUID = 1;
+    private final Set<Integer> set;
+    
+    SortedByAbsoluteValueIntegerSet() {
+        set = new LinkedHashSet<>();
+    }
+
+    @Override
+    public int size() {
+        return set.size();
+    }
+
+    @Override
+    public boolean add(Integer integer) {
+        return set.add(integer);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return set.remove(o);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return set.contains(o);
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+
+            private final List<Integer> setOfElements = set.stream().
+                    sorted(Comparator.comparingInt(Math::abs)).collect(Collectors.toList());
+            private int index;
+
+            @Override
+            public boolean hasNext() {
+                index++;
+                return index <= setOfElements.size();
+            }
+
+            @Override
+            public Integer next() {
+                return setOfElements.get(index - 1);
+            }
+
+        };
+    }
 
 }
